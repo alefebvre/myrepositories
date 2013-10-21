417ba934de955873c5ce57986da4f86ea127ab98
Zanderii <aurelien.lefebvre@viacesi.fr>
2013-10-21 09:45:30 +0200

    Script

--- push_to_gh_pages.sh
+++ push_to_gh_pages.sh
@@ -12,7 +12,7 @@ if [ "$POST_BUILD" == "true" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         # copy data we're interested in to other place
         cp -Rv build/result/coverage $HOME/build/coverage
         cp -Rv build/result/docs $HOME/build/docs
-        cp -Rv build/result/phploc.txt $HOME/build/phploc.txt
+      
         # go to home and setup git
         cd $HOME
         git config --global user.email "travis@travis-ci.org"
@@ -34,8 +34,8 @@ if [ "$POST_BUILD" == "true" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
  
 
         # copy stuff
-        cp -Rv $HOME/build/coverage/ coverage/
-        cp -Rv $HOME/build/docs/ docs/
+        cp -Rv $HOME/build/coverage/coverage/
+        cp -Rv $HOME/build/docs/docs/
 
 
         # add, commit and push files
