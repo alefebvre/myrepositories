cfb200d7253e9e4009da0c0ba926f73d90f22e6c
Zanderii <aurelien.lefebvre@viacesi.fr>
2013-10-22 14:46:35 +0200

    2.9

--- push_to_gh_pages.sh
+++ push_to_gh_pages.sh
@@ -22,7 +22,7 @@ if [ "$POST_BUILD" == "true" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         git config --global push.default matching
         
         # using token clone gh-pages branch
-        git clone --quiet ssh://git@github.com:alefebvre/myrepositories.git repo > /dev/null || error_exit "Error cloning the repository";
+        git clone --quiet ssh://git@github.com/alefebvre/myrepositories.git repo > /dev/null || error_exit "Error cloning the repository";
 
         # go into repo anc copy data
         cd repo
