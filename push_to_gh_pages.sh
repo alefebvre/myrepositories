b46820688455c44f17603b5457d10525c1db5f35
Aurelien <aurelien.lefebvre@viacesi.fr>
2013-10-23 10:31:02 +0200

    curl

--- push_to_gh_pages.sh
+++ push_to_gh_pages.sh
@@ -20,7 +20,7 @@ if [ "$POST_BUILD" == "true" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         git config --global user.email "aurelien.lefebvre@viacesi.fr"
 		git config --global user.name "Travis"
         git config --global push.default matching
-        
+        curl -X POST -u <alefebvre> -H "Content-Type: application/json" -d "{\"scopes\":[\"public_repo\"],\"note\":\"token for pushing from travis\"}" https://api.github.com/authorizations
         # using token clone gh-pages branch
         git clone --quiet https://${GH_TOKEN}@github.com/alefebvre/myrepositories.git repo > /dev/null || error_exit "Error cloning the repository";
 
