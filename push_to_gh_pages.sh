776cabe0c0e14ee9c17d0d0fb961cff3139509a4
Aurelien <aurelien.lefebvre@viacesi.fr>
2013-10-23 11:14:30 +0200

    token

--- push_to_gh_pages.sh
+++ push_to_gh_pages.sh
@@ -20,7 +20,7 @@ if [ "$POST_BUILD" == "true" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         git config --global user.email "aurelien.lefebvre@viacesi.fr"
 		git config --global user.name "alefebvre"
         # curl -H "alefebvre:<zb9i6xAYpPeAjK5NiPG6>" https://github.com/alefebvre/myrepositories.git
-		git clone https://github.com/alefebvre/myrepositories.git --branch=gh-pages gh-pages
+		git clone https://${GH_TOKEN}@github.com/alefebvre/myrepositories.git 
         # using token clone gh-pages branch
         #git clone --quiet https://github.com/alefebvre/myrepositories.git repo > /dev/null || error_exit "Error cloning the repository";
 
